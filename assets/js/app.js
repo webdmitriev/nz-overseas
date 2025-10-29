04de0754346bfa7445cebda6c01114d35ba32839
webdmitriev <webdmitriev@gmail.com>
2025-10-30 02:31:06 +0300

    block main (pc) is ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,42 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const isAnimation = document.querySelector('.is-animation');
 
+  //
+  animateGraphLine()
+  function animateGraphLine() {
+    const path = document.getElementById('graph-line');
+    const length = path.getTotalLength();
+
+    // Устанавливаем начальные значения для анимации
+    path.style.strokeDasharray = length;
+    path.style.strokeDashoffset = length;
+    path.style.transition = 'none';
+
+    // Анимация
+    let start = null;
+    const duration = 20000; // 4 секунды
+
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      const progress = timestamp - start;
+      const percent = Math.min(progress / duration, 1);
+
+      // Easing функция для плавности
+      const eased = easeOutCubic(percent);
+
+      path.style.strokeDashoffset = length * (1 - eased);
+
+      if (percent < 1) {
+        requestAnimationFrame(step);
+      }
+    }
+
+    requestAnimationFrame(step);
+  }
+  function easeOutCubic(t) {
+    return 1 - Math.pow(1 - t, 3);
+  }
+
   // animation screen
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => entry.isIntersecting ? entry.target.classList.add('with-animation-active') : entry.target.classList.remove('www'))
