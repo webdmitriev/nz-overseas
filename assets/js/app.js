1ad8bfb37975e1b95953479eef9a5a87ab6b4cba
webdmitriev <webdmitriev@gmail.com>
2025-10-29 10:32:15 +0300

    base header is ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
   console.log("webdmitriev");
 
+  // ******
+  // header
+  $(".header").on("click", ".burger", function () {
+    $(this).toggleClass("active")
+  })
+
 });
